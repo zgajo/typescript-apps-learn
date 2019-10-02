2ddef6f8cedda169e65c4cf3497ed9f4d91a1ad0
Darko Pranjic <dpranjic@iolap.com>
2019-10-02 19:47:11 +0200

    Display users

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,27 @@
 import React, { useState } from "react";
+import { useUsersQuery } from "../generated/graphql";
 
 interface Props {}
 
 export const Home: React.FC<Props> = () => {
-  const [data] = useState("Home");
-  return <div>{data}</div>;
+  const { data } = useUsersQuery({ fetchPolicy: "network-only" });
+
+  if (!data) {
+    return <div>Loading users . . . </div>;
+  }
+
+  return (
+    <div>
+      users:
+      <div>
+        <ul>
+          {data.users.map(user => (
+            <li key={user.id}>
+              {user.email}, {user.id}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
