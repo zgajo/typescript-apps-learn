14d1945b5327acaaf8f411332c749043e63a5e88
Darko Pranjic <dpranjic@iolap.com>
2019-10-05 12:07:02 +0200

    Cookie expiry fix

--- server/src/sendRefreshToken.ts
+++ server/src/sendRefreshToken.ts
@@ -1,8 +1,14 @@
 import { Response } from "express";
 
 export const sendRefreshToken = (res: Response, token: String) => {
+  const expiryCookieDate = new Date();
+  expiryCookieDate.setDate(expiryCookieDate.getDate() + 7);
+
   res.cookie("jid", token, {
     httpOnly: true,
+    // http://expressjs.com/en/4x/api.html#res.cookie
+    // Expiry date of the cookie in GMT. If not specified or set to 0, creates a session cookie.
+    expires: expiryCookieDate,
     path: "/refresh_token"
   });
 };
